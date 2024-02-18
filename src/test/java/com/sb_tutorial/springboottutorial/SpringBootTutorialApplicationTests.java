d2f9f2aed520b642799a4d79faa1aacd95e5c1d6
lavanchuan <chuan661999@gmail.com>
2024-02-18 15:22:00 +0700

    init project

---
+++ src/test/java/com/sb_tutorial/springboottutorial/SpringBootTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb_tutorial.springboottutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
